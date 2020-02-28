7a3de2351dcd440c01c03aa0294c85bdd30892f5
Sijia <sijia@10-19-119-113.dynapool.wireless.nyu.edu>
2020-02-28 16:49:36 -0500

    init

---
+++ Assets/scripts/playerCollision.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class playerCollision : MonoBehaviour
+{
+    public playerMovement movement;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "obstacle") {
+            movement.enabled = false;
+        }
+    }
+}
---
+++ Assets/scripts/playerMovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //    rb.AddForce(0,200,500);
+    //}
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // add a forward force
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        else if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+    }
+}
